1e78dca04b56e3001c0ec44f9795afb31b7909ad
Glitch (metric-imperial-converter-marco) <none>
2019-06-17 12:10:10 +0000

    🔋📫 Updated with Glitch

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -68,8 +68,15 @@ suite('Functional Tests', function() {
       });
       
       test('Convert kg (no number)', function(done) {
-        
-        //done();
+        chai.request(server)
+          .get("/api/convert")
+          .query({ input: "kg"})
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.body.initNum, 1);
+            assert.equal(res.body.initUnit, "kg");
+        })
+        done();
       });
       
     });
